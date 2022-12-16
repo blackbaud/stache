4fd9131c45ebb1ab8041da1da386e9895ef856ea
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-12-16 12:27:42 -0500

    fix tests

--- libs/stache/src/lib/modules/tutorial-step/tutorial-step-body.component.ts
+++ libs/stache/src/lib/modules/tutorial-step/tutorial-step-body.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'stache-tutorial-step-body',
   templateUrl: './tutorial-step-body.component.html',
   styleUrls: ['./tutorial-step-body.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StacheTutorialStepBodyComponent {}
--- libs/stache/src/lib/modules/tutorial-step/tutorial-step.component.spec.ts
+++ libs/stache/src/lib/modules/tutorial-step/tutorial-step.component.spec.ts
@@ -25,10 +25,17 @@ describe('StacheTutorialStepComponent', () => {
     expect(component.showNumber).toBe(true);
   });
 
-  // This will allow documentation writers to not worry about proper attribute binding.
-  it('should allow hiding numbers by setting `showNumber` to string "false"', () => {
-    (component as any).showNumber = 'false';
+  it('should reset the showNumber input', () => {
+    component.showNumber = false;
     fixture.detectChanges();
-    expect(component.showNumber).toEqual(false);
+    expect(
+      fixture.nativeElement.querySelector('.stache-tutorial-step')
+    ).not.toHaveClass('stache-tutorial-step-numbered');
+
+    component.showNumber = undefined;
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('.stache-tutorial-step')
+    ).toHaveClass('stache-tutorial-step-numbered');
   });
 });
