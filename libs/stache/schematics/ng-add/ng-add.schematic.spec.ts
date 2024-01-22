a5661a44056608ce0597c8d0dec2b0358c126989
John White <750350+johnhwhite@users.noreply.github.com>
2024-01-22 10:17:16 -0500

    Update workflow to work like SKY UX. Update test.

--- libs/stache/schematics/ng-add/ng-add.schematic.spec.ts
+++ libs/stache/schematics/ng-add/ng-add.schematic.spec.ts
@@ -40,7 +40,7 @@ describe('ng-add.schematic', () => {
     ];
 
     for (const packageName of packageNames) {
-      expect(packageJson.dependencies[packageName]).toEqual('^9.0.0');
+      expect(packageJson.dependencies[packageName]).toEqual('^9.21.2');
     }
   });
 });
