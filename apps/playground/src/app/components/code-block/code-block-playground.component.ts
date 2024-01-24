bb54bb6251171e3dfe60d83d94116bce6352e374
John White <750350+johnhwhite@users.noreply.github.com>
2024-01-24 12:37:50 -0500

    Fix code block example. Update test.

--- apps/playground/src/app/components/code-block/code-block-playground.component.ts
+++ apps/playground/src/app/components/code-block/code-block-playground.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-code-block-playground',
   templateUrl: './code-block-playground.component.html',
+  preserveWhitespaces: true,
 })
 export class CodeBlockPlaygroundComponent {}
