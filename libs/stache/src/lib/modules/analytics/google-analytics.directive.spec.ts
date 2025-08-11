564ed40b5480899275a337661fb1e82ecf2c5c34
Steve Brush <steve.brush@blackbaud.com>
2025-08-11 15:33:32 -0400

    fix(stache): respect strict CSP when injecting Google Analytics tag manager (#121)

--- libs/stache/src/lib/modules/analytics/google-analytics.directive.spec.ts
+++ libs/stache/src/lib/modules/analytics/google-analytics.directive.spec.ts
@@ -1,3 +1,4 @@
+import { CSP_NONCE } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
@@ -21,16 +22,26 @@ describe('StacheGoogleAnalyticsDirective', () => {
 
   class MockWindowService {
     public nativeWindow = {
-      eval: function () {
-        this.ga = () => true;
-      },
       ga: false,
+      dataLayer: undefined,
       document: {
         getElementById: jasmine
           .createSpy('getElementById')
-          .and.callFake(function (id: any) {
+          .and.callFake(function () {
             return false;
           }),
+        createElement: jasmine
+          .createSpy('createElement')
+          .and.callFake(function () {
+            const element = {
+              textContent: '',
+              setAttribute: jasmine.createSpy('setAttribute'),
+            };
+            return element;
+          }),
+        head: {
+          appendChild: jasmine.createSpy('appendChild'),
+        },
       },
     };
   }
@@ -69,6 +80,7 @@ describe('StacheGoogleAnalyticsDirective', () => {
         { provide: SkyAppConfig, useValue: mockConfigService },
         { provide: StacheWindowRef, useValue: mockWindowService },
         { provide: Router, useValue: mockRouter },
+        { provide: CSP_NONCE, useValue: null },
       ],
     }).compileComponents();
 
@@ -173,5 +185,256 @@ describe('StacheGoogleAnalyticsDirective', () => {
     spyOn(directiveInstance, 'addGoogleTagManagerScript').and.callThrough();
     fixture.detectChanges();
     expect(directiveInstance.addGoogleTagManagerScript).not.toHaveBeenCalled();
+  });
+
+  it('should create and append script tag with correct attributes', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+
+    const mockScript = {
+      setAttribute: jasmine.createSpy('setAttribute'),
+    };
+
+    mockWindowService.nativeWindow.document.createElement.and.returnValue(
+      mockScript,
+    );
+
+    directiveInstance.addGoogleTagManagerScript();
+
+    expect(
+      mockWindowService.nativeWindow.document.createElement,
+    ).toHaveBeenCalledWith('script');
+
+    expect(mockScript.setAttribute).toHaveBeenCalledWith('async', 'async');
+    expect(mockScript.setAttribute).toHaveBeenCalledWith(
+      'src',
+      'https://www.googletagmanager.com/gtm.js?id=GTM-W56QP9',
+    );
+
+    expect(mockWindowService.nativeWindow.dataLayer).toBeDefined();
+    expect(mockWindowService.nativeWindow.dataLayer.length).toBe(1);
+    expect(
+      mockWindowService.nativeWindow.dataLayer[0]['gtm.start'],
+    ).toBeDefined();
+    expect(mockWindowService.nativeWindow.dataLayer[0].event).toBe('gtm.js');
+
+    expect(
+      mockWindowService.nativeWindow.document.head.appendChild,
+    ).toHaveBeenCalledWith(mockScript);
+  });
+
+  it('should set nonce attribute on script if CSP_NONCE is provided', () => {
+    TestBed.resetTestingModule();
+    TestBed.configureTestingModule({
+      declarations: [StacheGoogleAnalyticsTestComponent],
+      imports: [StacheAnalyticsModule],
+      providers: [
+        { provide: SkyAppConfig, useValue: mockConfigService },
+        { provide: StacheWindowRef, useValue: mockWindowService },
+        { provide: Router, useValue: mockRouter },
+        { provide: CSP_NONCE, useValue: 'test-nonce-123' },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StacheGoogleAnalyticsTestComponent);
+    directiveElement = fixture.debugElement.query(
+      By.directive(StacheGoogleAnalyticsDirective),
+    );
+
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+
+    const mockScript = {
+      setAttribute: jasmine.createSpy('setAttribute'),
+    };
+
+    mockWindowService.nativeWindow.document.createElement.and.returnValue(
+      mockScript,
+    );
+
+    directiveInstance.addGoogleTagManagerScript();
+
+    expect(mockScript.setAttribute).toHaveBeenCalledWith(
+      'nonce',
+      'test-nonce-123',
+    );
+  });
+
+  it('should not set nonce attribute on script if CSP_NONCE is null', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+
+    const mockScript = {
+      setAttribute: jasmine.createSpy('setAttribute'),
+    };
+
+    mockWindowService.nativeWindow.document.createElement.and.returnValue(
+      mockScript,
+    );
+
+    directiveInstance.addGoogleTagManagerScript();
+
+    expect(mockScript.setAttribute).not.toHaveBeenCalledWith(
+      'nonce',
+      jasmine.any(String),
+    );
+  });
+
+  it('should use custom tagManagerContainerId in script src', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+
+    mockConfigService.skyux.appSettings = {
+      stache: {
+        googleAnalytics: {
+          tagManagerContainerId: 'GTM-CUSTOM',
+        },
+      },
+    };
+
+    directiveInstance.updateDefaultConfigs();
+
+    const mockScript = {
+      setAttribute: jasmine.createSpy('setAttribute'),
+    };
+
+    mockWindowService.nativeWindow.document.createElement.and.returnValue(
+      mockScript,
+    );
+
+    directiveInstance.addGoogleTagManagerScript();
+
+    expect(mockScript.setAttribute).toHaveBeenCalledWith(
+      'src',
+      'https://www.googletagmanager.com/gtm.js?id=GTM-CUSTOM',
+    );
+  });
+
+  it('should call initGoogleAnalytics and bindPageViewsToRouter when conditions are met', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    spyOn(directiveInstance, 'addGoogleTagManagerScript').and.callThrough();
+    spyOn(directiveInstance, 'initGoogleAnalytics').and.callThrough();
+    spyOn(directiveInstance, 'bindPageViewsToRouter').and.callThrough();
+
+    directiveInstance.ngOnInit();
+
+    expect(directiveInstance.addGoogleTagManagerScript).toHaveBeenCalled();
+    expect(directiveInstance.initGoogleAnalytics).toHaveBeenCalled();
+    expect(directiveInstance.bindPageViewsToRouter).toHaveBeenCalled();
+  });
+
+  it('should initialize Google Analytics without using eval', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockWindowService.nativeWindow.ga = undefined;
+    mockWindowService.nativeWindow['GoogleAnalyticsObject'] = undefined;
+
+    directiveInstance.initGoogleAnalytics();
+
+    expect(mockWindowService.nativeWindow['GoogleAnalyticsObject']).toBe('ga');
+    expect(mockWindowService.nativeWindow.ga).toBeDefined();
+    expect(typeof mockWindowService.nativeWindow.ga).toBe('function');
+    expect(mockWindowService.nativeWindow.ga.l).toEqual(jasmine.any(Number));
+    expect(mockWindowService.nativeWindow.ga.q).toEqual(jasmine.any(Array));
+  });
+
+  it('should call ga create after initializing Google Analytics', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockWindowService.nativeWindow.ga = undefined;
+    mockWindowService.nativeWindow['GoogleAnalyticsObject'] = undefined;
+
+    directiveInstance.initGoogleAnalytics();
+
+    // The ga function should have been called with create
+    expect(mockWindowService.nativeWindow.ga.q.length).toBeGreaterThan(0);
+    const firstCall = mockWindowService.nativeWindow.ga.q[0];
+    expect(firstCall[0]).toBe('create');
+    expect(firstCall[1]).toBe('UA-2418840-1');
+    expect(firstCall[2]).toBe('auto');
+  });
+
+  it('should track page views for NavigationEnd events', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockWindowService.nativeWindow.ga = jasmine.createSpy('ga');
+    directiveInstance['appName'] = 'test-app';
+
+    directiveInstance.bindPageViewsToRouter();
+
+    // Simulate a NavigationEnd event
+    const navigationEndEvent = new NavigationEnd(1, '/test-url', '/test-url');
+    mockRouter.events = observableOf(navigationEndEvent);
+    directiveInstance.bindPageViewsToRouter();
+
+    expect(mockWindowService.nativeWindow.ga).toHaveBeenCalledWith(
+      'set',
+      'page',
+      'test-app/test-url',
+    );
+    expect(mockWindowService.nativeWindow.ga).toHaveBeenCalledWith(
+      'send',
+      'pageview',
+    );
+  });
+
+  it('should handle enabled setting as boolean false', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockConfigService.skyux.appSettings = {
+      stache: {
+        googleAnalytics: {
+          enabled: false,
+        },
+      },
+    };
+
+    directiveInstance.updateDefaultConfigs();
+
+    expect(directiveInstance['isEnabled']).toBe(false);
+  });
+
+  it('should handle enabled setting as string "false"', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockConfigService.skyux.appSettings = {
+      stache: {
+        googleAnalytics: {
+          enabled: 'false',
+        },
+      },
+    };
+
+    directiveInstance.updateDefaultConfigs();
+
+    expect(directiveInstance['isEnabled']).toBe(false);
+  });
+
+  it('should keep enabled as true for other values', () => {
+    const directiveInstance = directiveElement.injector.get(
+      StacheGoogleAnalyticsDirective,
+    );
+    mockConfigService.skyux.appSettings = {
+      stache: {
+        googleAnalytics: {
+          enabled: 'true',
+        },
+      },
+    };
+
+    directiveInstance.updateDefaultConfigs();
+
+    expect(directiveInstance['isEnabled']).toBe(true);
   });
 });
