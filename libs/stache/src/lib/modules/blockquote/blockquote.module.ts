1e847dd0a38659f30ae157cbb9d87bbefe07f3b5
Trevor Burch <Trevor.Burch@blackbaud.com>
2025-09-05 15:06:25 -0400

    fix(stache): blockquote component uses `sky-icon` (#137)

--- libs/stache/src/lib/modules/blockquote/blockquote.module.ts
+++ libs/stache/src/lib/modules/blockquote/blockquote.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SkyIconModule } from '@skyux/icon';
 
 import { StacheBlockquoteComponent } from './blockquote.component';
 
 @NgModule({
   declarations: [StacheBlockquoteComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SkyIconModule],
   exports: [StacheBlockquoteComponent],
 })
 export class StacheBlockquoteModule {}
