18d3f63ac01dbbfdea0015a69bb8a9475fef2189
Steve Brush <steve.brush@blackbaud.com>
2025-09-08 11:08:44 -0400

    fix(stache): fix StacheTitleService to allow undefined app title (#139)

--- libs/stache/src/lib/modules/wrapper/title.service.ts
+++ libs/stache/src/lib/modules/wrapper/title.service.ts
@@ -1,23 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { SkyAppConfig } from '@skyux/config';
 
 @Injectable()
 export class StacheTitleService {
-  constructor(
-    private title: Title,
-    private configService: SkyAppConfig,
-  ) {}
+  readonly #title = inject(Title);
+  readonly #appConfig = inject(SkyAppConfig);
 
   public setTitle(...parts: string[]): void {
-    let windowTitle = this.configService.skyux.app.title;
+    const appTitle = this.#appConfig.skyux.app?.title;
 
     if (parts && parts.length > 0) {
-      parts.push(windowTitle);
-      const validParts = parts.filter((part: string) => !!part && part.trim());
-      windowTitle = validParts.join(' - ');
+      if (appTitle) {
+        parts.push(appTitle);
+      }
+
+      const validParts = parts.filter((part) => !!part && part.trim());
+
+      if (validParts.length > 0) {
+        this.#title.setTitle(validParts.join(' - '));
+
+        return;
+      }
     }
 
-    this.title.setTitle(windowTitle);
+    this.#title.setTitle(appTitle ?? 'Blackbaud');
   }
 }
